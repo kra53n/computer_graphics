bbca11f7a0883939379f79dac4514db554b3b029
Krai53n <greasha46@gmail.com>
2024-04-23 01:16:20 +0300

    mlg pro gamer in that house

--- shaders/main.cpp
+++ shaders/main.cpp
@@ -1,3 +1,4 @@
+#define _USE_MATH_DEFINES
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,6 +7,7 @@
 #include <GLFW/glfw3.h>
 
 #include "shader_program.hpp"
+
 
 GLFWwindow* create_window()
 {
@@ -62,11 +64,11 @@ int main()
 
 	//
 
-	float vertices[] = {
-		 .0f,         .5f, .0f,  1.f, .0f, .0f,
-		 0.354f,  -0.354f, .0f,  .0f, 1.f, .0f,
-		 -0.354f, -0.354f, .0f,  .0f, .0f, 1.f
-	};
+	//float vertices[] = {
+	//	 .0f,         .5f, .0f,  1.f, .0f, .0f,
+	//	 0.354f,  -0.354f, .0f,  .0f, 1.f, .0f,
+	//	 -0.354f, -0.354f, .0f,  .0f, .0f, 1.f
+	//};
 
 	unsigned VBO, VAO;
 	glGenVertexArrays(1, &VAO);
@@ -74,7 +76,7 @@ int main()
 	glBindVertexArray(VAO);
 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	//glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
@@ -87,9 +89,40 @@ int main()
 
 	while (!glfwWindowShouldClose(window))
 	{
+		float time = glfwGetTime();
+		// 1
+		//float x = sin(time * M_PI);
+		//float y = sin(time * M_PI / 3);
+		//float z = sin(time * M_PI / 3 * 2);
+		// 2
+		//float v = abs(sin(time/4));
+		//float x = v <= 1. / 3 ? v * 3 : 0;
+		//float y = v > 1. / 3 and v <= 2. / 3 ? (v - 1. / 3) * 3 : 0;
+		//float z = v > 2. / 3 ? (v - 2. / 3) * 3 : 0;
+		// 3
+		//float v = abs(sin(time/4));
+		//float x = v <= 1. / 3 ? v * 3 : 0;
+		//float y = v > 1. / 3 and v <= 2. / 3 ? (v - 1. / 3) * 3 : 0;
+		//float z = v > 2. / 3 ? (v - 2. / 3) * 3 : 0;
+		//x = x == 0 ? (0.8*y + 0.2*z) / 8 : x;
+		//y = y == 0 ? (0.2*x + 0.8*z) / 12 : y;
+		//z = z == 0 ? (0.8*x + 0.2*y) / 6 : z;
+		// 4 - finale
+		float x = sin(time);
+		float y = sin(time + M_PI / 3);
+		float z = sin(time - M_PI / 3);
+		// it is also good idea to play with x y z in vertices
+		float vertices[] = {
+				 .0f,         .5f, .0f,  x, y, z,
+				 0.354f,  -0.354f, .0f,  z, x, y,
+				 -0.354f, -0.354f, .0f,  y, z, x
+		};
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+		shader_program.set("time", (float)glfwGetTime());
+
 		process_input(window);
 
-		glClearColor(.2, .3, .3, 1.);
+		glClearColor(.2f, .3f, .3f, 1.f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		glBindVertexArray(VAO);
