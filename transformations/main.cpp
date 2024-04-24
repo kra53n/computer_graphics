caaca1f9266aad9930e28cf821eeabc43df82716
Krai53n <greasha46@gmail.com>
2024-04-25 00:10:26 +0300

    add transformations

---
+++ transformations/main.cpp
@@ -0,0 +1,184 @@
+#define _USE_MATH_DEFINES
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
+#include "shader_program.hpp"
+
+
+GLFWwindow* create_window()
+{
+	GLFWwindow* window = glfwCreateWindow(800, 600, "window", NULL, NULL);
+	if (window == NULL)
+	{
+		printf("Failed to create GLFW window");
+		glfwTerminate();
+		exit(-1);
+	}
+}
+
+void load_glad()
+{
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		printf("Failed to init GLAD");
+		exit(-1);
+	}
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int wdt, int hgt)
+{
+	glViewport(0, 0, wdt, hgt);
+}
+
+void process_input(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, true);
+	}
+}
+
+int main()
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	auto window = create_window();
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	load_glad();
+
+	//
+
+	ShaderProgram shader_program({
+		Shader("shaders/shader.vs"),
+		Shader("shaders/shader.fs"),
+	});
+
+	//
+
+	float vertices[] = {
+		-0.5f, -0.5f,  0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,
+		-0.5f,  0.5f,  0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f,
+		 0.5f,  0.5f,  0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,
+							   
+		-0.5f, -0.5f,  0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,
+		0.5f,   0.5f,  0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,
+		0.5f,  -0.5f,  0.0f,   0.0f, 0.1f, 0.0f,   1.0f, 0.0f,
+	};
+
+	//
+
+	float tex_coords[] = {
+		0.0f, 0.0f,
+		0.5f, 1.0f,
+		1.0f, 0.0f,
+	};
+	unsigned texture, texture1;
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	int width, height, nr_channels;
+	stbi_set_flip_vertically_on_load(true);
+	unsigned char* data = stbi_load("assets/wall.jpg", &width, &height, &nr_channels, 0);
+	if (data)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		printf("Failed to load texture\n");
+	}
+	stbi_image_free(data);
+
+	glGenTextures(1, &texture1);
+	glBindTexture(GL_TEXTURE_2D, texture1);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	data = stbi_load("assets/awesomeface.png", &width, &height, &nr_channels, 0);
+	if (data)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		printf("Failed to load texture\n");
+	}
+	stbi_image_free(data);
+
+	//
+
+	unsigned VBO, VAO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+	glEnableVertexAttribArray(2);
+
+	//
+
+	shader_program.use();
+	shader_program.set("texture0", 0);
+	shader_program.set("texture1", 1);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		process_input(window);
+
+		glClearColor(.2f, .3f, .3f, 1.f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, texture);
+		glActiveTexture(GL_TEXTURE1);
+		glBindTexture(GL_TEXTURE_2D, texture1);
+
+		glm::mat4 transform = glm::mat4(1.0f);
+		transform = glm::translate(transform, glm::vec3(0.5f, -0.5f, 0.0f));
+		transform = glm::rotate(transform, (float)glfwGetTime(), glm::vec3(1.0f, 1.0f, 1.0f));
+		printf("%f\n", (float)glfwGetTime());
+
+
+		shader_program.use();
+		GLuint trans_loc = glGetUniformLocation(shader_program.ID(), "transform");
+		glUniformMatrix4fv(trans_loc, 1, GL_FALSE, glm::value_ptr(transform));
+
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+
+	glfwTerminate();
+}
---
+++ transformations/shader_program.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <vector>
+
+#include <glad/glad.h>
+
+#include "shader.hpp"
+
+class ShaderProgram
+{
+	GLuint _ID;
+	std::vector<Shader> _shaders;
+
+	void _link_program();
+	void _compile();
+public:
+
+	ShaderProgram(std::vector<Shader> shaders);
+	~ShaderProgram();
+	GLuint ID();
+	void use();
+
+	void set(const char* uniform_name, bool val);
+	void set(const char* inform_name, int val);
+	void set(const char* uniform_name, float val);
+	void set(const char* uniform_name, const std::vector<float>& vals);
+};
