509ff012831ecf698073c1fe3e54a7640557f2ea
Krai53n <greasha46@gmail.com>
2024-04-25 13:50:52 +0300

    add header for camera class

---
+++ camera/camera.hpp
@@ -0,0 +1,66 @@
+#pragma once
+
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+
+class Camera
+{
+public:
+	const float
+		YAW = -90.0f,
+		PITCH = 0.0f,
+		SPEED = 2.5f,
+		MOUSE_SENSETIVITY = 0.1f,
+		ZOOM = 45.0f;
+
+	enum Move
+	{
+		FORW,
+		BACK,
+		LEFT,
+		RIGHT,
+	};
+
+private:
+	glm::vec3
+		_pos,
+		_front,
+		_up,
+		_right,
+		_world_up;
+
+	float
+		_yaw,
+		_pitch,
+		_movement_spped,
+		_mouse_sensetivity,
+		_zoom;
+
+public:
+	Camera();
+
+	Camera(glm::vec3 pos);
+	Camera(float pos_x, float pos_y, float pos_z);
+
+	Camera(glm::vec3 pos, glm::vec3 up);
+	Camera(float pos_x, float pos_y, float pos_z, float up_x, float up_y, float up_z);
+
+	Camera(glm::vec3 pos, glm::vec3 up, float yaw, float pitch);
+	Camera(float pos_x, float pos_y, float pos_z, float up_x, float up_y, float up_z, float yaw, float pitch);
+
+	Camera(float yaw, float pitch);
+
+	void set_pos(glm::vec3 vec);
+	void set_pos(float x, float y, float z);
+	void set_up(glm::vec3 vec);
+	void set_up(float x, float y, float z);
+	void set_yaw(float val);
+	void set_pitch(float val);
+
+	void process_keyboard(Move direction, float dt);
+	void process_mouse_movement(float x_offset, float y_offset, GLboolean constrain_pitch = true);
+	void process_mouse_scroll(float y_offset);
+
+private:
+	void _update();
+};
---
+++ camera/shader_program.cpp
@@ -0,0 +1,81 @@
+#include "shader_program.hpp"
+
+#include <exception>
+
+ShaderProgram::ShaderProgram(std::vector<Shader> shaders)
+{
+    _shaders = shaders;
+	_compile();
+}
+
+ShaderProgram::~ShaderProgram()
+{
+    glDeleteProgram(_ID);
+}
+
+GLuint ShaderProgram::ID()
+{
+    return _ID;
+}
+
+void ShaderProgram::_compile()
+{
+    _ID = glCreateProgram();
+    for (Shader shader : _shaders) shader.attach(_ID);
+    _link_program();
+    for (Shader shader : _shaders) shader.del();
+}
+
+void ShaderProgram::_link_program()
+{
+    glLinkProgram(_ID);
+    int success;
+    char info_log[512];
+    glGetProgramiv(_ID, GL_LINK_STATUS, &success);
+    if (!success)
+    {
+        glGetProgramInfoLog(_ID, 512, NULL, info_log);
+        printf("ERROR::SHADER::PROGRAM::LINKING_FAILED\n%s\n", info_log);
+    }
+}
+
+void ShaderProgram::use()
+{
+    glUseProgram(_ID);
+}
+
+#define fill_uniform(func_name, ...) \
+    func_name(glGetUniformLocation(_ID, uniform_name), __VA_ARGS__)
+
+void ShaderProgram::set(const char* uniform_name, bool val)
+{
+    fill_uniform(glUniform1i, (int)val);
+}
+
+void ShaderProgram::set(const char* uniform_name, int val)
+{
+    fill_uniform(glUniform1i, val);
+}
+
+void ShaderProgram::set(const char* uniform_name, float val)
+{
+    fill_uniform(glUniform1f, val);
+}
+
+void ShaderProgram::set(const char* uniform_name, const std::vector<float>& vals)
+{
+    switch (vals.size())
+    {
+    case 1: fill_uniform(glUniform1f, vals[0]); break;
+    case 2: fill_uniform(glUniform2f, vals[0], vals[1]); break;
+    case 3: fill_uniform(glUniform3f, vals[0], vals[1], vals[3]); break;
+    case 4: fill_uniform(glUniform4f, vals[0], vals[1], vals[3], vals[4]); break;
+    default:
+        throw std::exception("invalid size for uniform float vec");
+    }
+}
+
+void ShaderProgram::set(const char* uniform_name, const glm::mat4& val)
+{
+    fill_uniform(glUniformMatrix4fv, 1, GL_FALSE, &val[0][0]);
+}
